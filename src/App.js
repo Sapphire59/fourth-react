9a0b16b208656cff6ced4f9c05b7c0970b06c521
Deborah <liz.joy43@gmail.com>
2024-12-05 08:43:16 -0700

    added li and img

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>My Weather App</h1>
-
+      <Weather />
       <footer>
         This project was coded by
-        <a href="https://github.com/Sapphire59" target="_blank">
+        <a
+          href="https://github.com/Sapphire59"
+          rel="noreferrer"
+          target="_blank"
+        >
           {" "}
           Deborah Chacko{" "}
         </a>
         is open-sourced on{" "}
-        <a href="https://github.com/Sapphire59/fourth-react" target="_blank">
+        <a
+          href="https://github.com/Sapphire59/fourth-react"
+          rel="noreferrer"
+          target="_blank"
+        >
           Github
         </a>{" "}
         and is hosted on{" "}
         <a
           href="https://deborah-weather-react-project.netlify.app/"
+          rel="noreferrer"
           target="_blank"
         >
           Netlify
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,27 @@
 import React from "react";
 
+import "./Weather.css";
+
 function Weather() {
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
 
-return(
-<div>
-    
-</div>
-);
+      <ul>
+        <li> Thursday 12:00 AM</li>
+        <li>Rain and snow</li>
+      </ul>
 
-
-
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/snow_s_rain.png"
+            alt="Rain and snow"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Weather;
