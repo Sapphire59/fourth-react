c46c2e3fb65801dad72c68425ba9882986be8f36
Deborah <liz.joy43@gmail.com>
2024-12-04 21:56:56 -0700

    added Bootstrap with footer and links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ function App() {
   return (
     <div className="App">
       <h1>My Weather App</h1>
+
+      <footer>
+        This project was coded by
+        <a href="https://github.com/Sapphire59" target="_blank">
+          {" "}
+          Deborah Chacko{" "}
+        </a>
+        is open-sourced on{" "}
+        <a href="https://github.com/Sapphire59/fourth-react" target="_blank">
+          Github
+        </a>{" "}
+        and is hosted on{" "}
+        <a
+          href="https://deborah-weather-react-project.netlify.app/"
+          target="_blank"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
